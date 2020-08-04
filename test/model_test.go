8a6681b58676506708dae5cdc9dee0eafe403223
wangqian <wangqian@devopsdeMacBook-Pro.local>
2020-08-04 14:29:28 +0800

    update test

--- test/model_test.go
+++ test/model_test.go
@@ -9,7 +9,7 @@ func TestUser(t *testing.T) {
 	t.Logf("查询user by name：%+v\n", model.SelectUserByName("mike"))
 	t.Logf("创建user记录：%+v\n", model.InsertUser("max"))
 
-	t.Logf("查询某条关系：%+v\n", model.GetOneRelation(1, 11))
-	t.Logf("创建一个关系：%+v\n", model.InsertRelationship(11, 1, "disliked"))
-	t.Logf("更改某个关系：%+v\n", model.UpdateRelation(11, 1, "liked"))
+	t.Logf("查询某条关系：%+v\n", model.DefaultRelationModel.GetOneRelation(1, 11))
+	t.Logf("创建一个关系：%+v\n", model.DefaultRelationModel.InsertRelationship(11, 1, "disliked"))
+	t.Logf("更改某个关系：%+v\n", model.DefaultRelationModel.UpdateRelation(11, 1, "liked"))
 }
